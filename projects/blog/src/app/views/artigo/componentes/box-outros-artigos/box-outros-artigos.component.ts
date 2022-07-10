301604bf5f1d7816212257c567100799fc7a285c
Jonas Sene Torres <jonas.sene.torres@gmail.com>
2022-07-09 22:19:32 -0300

    AJUSTES: Box outroa artigos

--- projects/blog/src/app/views/artigo/componentes/box-outros-artigos/box-outros-artigos.component.ts
+++ projects/blog/src/app/views/artigo/componentes/box-outros-artigos/box-outros-artigos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'jt-box-outros-artigos',
@@ -6,8 +6,42 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./box-outros-artigos.component.scss']
 })
 export class BoxOutrosArtigosComponent {
-  @Input() listaArtigoSerie: any[] = [];
+  _listaArtigoSerie: any[] = [];
+  @Input() artigoAtual: string = '';
+  desabilitarBotaoVoltar = false;
+  desabilitarBotaoAvancar = false;
+
+  get listaArtigoSerie(): any[] {
+    return this._listaArtigoSerie;
+  }
+
+  @Input() set listaArtigoSerie(value: any[]) {
+    this._listaArtigoSerie = value;
+
+    const ultimoIndiceLista = this.artigoAtual.length - 1;
+    const index = this.encontrarIndiceAtual();
+
+    this.desabilitarBotaoVoltar = false;
+    this.desabilitarBotaoAvancar = false;
+
+    if (index === 0) {
+      console.log('desabilitei')
+      this.desabilitarBotaoVoltar = true;
+      return;
+    }
+
+    if (index === ultimoIndiceLista) {
+      this.desabilitarBotaoAvancar = true;
+      return;
+    }
+  }
 
   constructor() { }
 
+  private encontrarIndiceAtual() {
+    return this.listaArtigoSerie.findIndex(
+      item => {
+        return item.titulo === this.artigoAtual;
+      });
+  }
 }
