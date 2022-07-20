f239246b6e91ba3bcd076b3782a6647c21253341
Jonas Sene Torres <jonas.sene.torres@gmail.com>
2022-07-20 02:35:58 -0300

    AJUSTES: Página home

--- projects/blog/src/app/views/home/home.component.ts
+++ projects/blog/src/app/views/home/home.component.ts
@@ -4,8 +4,10 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
+import { Observable, forkJoin } from 'rxjs';
 
 import { ArtigosService } from './../../services/artigos/artigos.service';
+import { CategoriasService } from '../../services/categorias/categorias.service';
 
 @Component({
   selector: 'jt-home',
@@ -13,24 +15,89 @@ import { ArtigosService } from './../../services/artigos/artigos.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  post: any;
-  post2: any;
-  @ViewChild('dataContainer') dataContainer: ElementRef = new ElementRef({});
-
-  loadData(data: any) {
-    this.dataContainer.nativeElement.innerHTML = data;
-  }
+  dadosArtigo: any;
+  categorias: any[] = [];
+  subcategorias: any = [];
+  ultimosArtigos: any[] = [];
+  artigosDaCategoria: any[] = [];
 
   constructor(
-    private artigosService: ArtigosService
-  ) // private sanitizer: DomSanitizer
-  {}
+    // private router: Router,
+    private artigosService: ArtigosService,
+    private categoriasService: CategoriasService,
+  ) {}
 
   ngOnInit(): void {
-    this.artigosService.obter(1).subscribe((sucesso: any) => {
-      this.post = sucesso[0].text;
+    this.obterDadosIniciais(1).subscribe((sucesso) => {
+      this.dadosArtigo = sucesso.artigo;
+      this.categorias = sucesso.categorias;
+      this.ultimosArtigos = sucesso.ultimosArtigos;
+      this.subcategorias = sucesso.subcategorias;
+      this.artigosDaCategoria = this.montarArtigosDaCategoria(this.subcategorias, sucesso.artigosDaCategoria);
+      // for (let indice = 0; indice < this.ultimosArtigos.length; indice++) {
+      //   const element = this.ultimosArtigos[indice];
+
+      // }
+
+      console.log( this.artigosDaCategoria);
+    },
+    (erro: any) => {
+      console.log(erro);
     });
   }
+
+  private obterDadosIniciais(idCategoria: number): Observable<any> {
+    const obterArtigo = this.categoriasService.obter(idCategoria);
+    const obterCategorias = this.categoriasService.listar();
+    // const obterSubcategorias = this.subcategoriasService.listar();
+    const obterUltimosArtigos = this.artigosService.listarUltimosArtigos();
+    const obterArtigosDaCategoria = this.artigosService.listarArtigosCategoria(idCategoria);
+
+    return forkJoin({
+      artigo: obterArtigo,
+      categorias: obterCategorias,
+      // subcategorias: obterSubcategorias,
+      ultimosArtigos: obterUltimosArtigos,
+      artigosDaCategoria: obterArtigosDaCategoria,
+    });
+  }
+
+  private montarArtigosDaCategoria(subcategorias: any[], artigosDaCategoria: any[]) {
+    let output = [];
+    for (const item of subcategorias) {
+      const subcategoriaItem = item.nome;
+      const colecaoArtigos = artigosDaCategoria.filter(
+        artigo => artigo.subCategoriaId === item.id
+      )
+
+      output.push({
+        subcategoria: subcategoriaItem,
+        artigos: colecaoArtigos
+      });
+    }
+
+    console.log('output', output);
+
+    return output;
+  }
+  // post: any;
+  // post2: any;
+  // @ViewChild('dataContainer') dataContainer: ElementRef = new ElementRef({});
+
+  // loadData(data: any) {
+  //   this.dataContainer.nativeElement.innerHTML = data;
+  // }
+
+  // constructor(
+  //   private artigosService: ArtigosService
+  // ) // private sanitizer: DomSanitizer
+  // {}
+
+  // ngOnInit(): void {
+  //   this.artigosService.obter(1).subscribe((sucesso: any) => {
+  //     this.post = sucesso[0].text;
+  //   });
+  // }
 
   // ngAfterViewInit() {
   //   this.postsService.get().subscribe(
