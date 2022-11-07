3be880ab64b38488fc1ecb8d79b1d9661e97532e
Jonas Sene Torres <jonas.sene.torres@gmail.com>
2022-11-06 21:44:27 -0300

    AJUSTES: contraste breadcrumb

--- projects/guide-dog/src/lib/components/breadcrumb/breadcrumb.component.ts
+++ projects/guide-dog/src/lib/components/breadcrumb/breadcrumb.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Injector, Input } from '@angular/core';
 
 import { BreadcrumbsItem } from '../../types/breadcrumbs-item.type';
+import { MasterBaseComponent } from '../master-base/master-base.component';
 
 @Component({
   selector: 'gd-breadcrumb',
   templateUrl: './breadcrumb.component.html',
   styleUrls: ['./breadcrumb.component.scss'],
 })
-export class BreadcrumbComponent {
+export class BreadcrumbComponent extends MasterBaseComponent {
   @Input() breadcrumbsItem: BreadcrumbsItem[] = [];
 
-  constructor() {}
+  constructor(protected override injector: Injector) {
+    super(injector);
+  }
 }
