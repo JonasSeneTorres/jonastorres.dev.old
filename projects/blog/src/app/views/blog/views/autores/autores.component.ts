62e1825bc7356b61c59db0b2506f8ede79e8efbb
Jonas Sene Torres <jonas.sene.torres@gmail.com>
2022-11-20 23:56:17 -0300

    INCLUSÃO: Darkmode tela inicial

--- projects/blog/src/app/views/blog/views/autores/autores.component.ts
+++ projects/blog/src/app/views/blog/views/autores/autores.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { JonastorresRoutes } from 'projects/blog/src/app/enuns/jonastorres-routes.enum';
 import { BlogService } from 'projects/blog/src/app/services/blog/blog.service';
+import { MasterBaseComponent } from 'projects/guide-dog/src/lib/components/master-base/master-base.component';
 import { BreadcrumbsItem } from 'projects/guide-dog/src/lib/types/breadcrumbs-item.type';
-import { Subject } from 'rxjs';
 
 import { JumbotronService } from './../../../../services/jumbotron/jumbotron.service';
 
@@ -10,13 +10,16 @@ import { JumbotronService } from './../../../../services/jumbotron/jumbotron.ser
   templateUrl: './autores.component.html',
   styleUrls: ['./autores.component.scss'],
 })
-export class AutoresComponent implements OnDestroy {
-  private _destroy$: Subject<boolean> = new Subject<boolean>();
-
+export class AutoresComponent extends MasterBaseComponent {
   breadcrumbsItem: BreadcrumbsItem[];
 
-  constructor(private _blogService: BlogService, private _jumbotronService: JumbotronService) {
-    this._blogService.tornarBoxPrincipalTransparente(false);
+  constructor(
+    private _blogService: BlogService,
+    private _jumbotronService: JumbotronService,
+    protected override injector: Injector
+  ) {
+    super(injector);
+    this._blogService.tornarBoxPrincipalTransparente(true);
     this.breadcrumbsItem = [JonastorresRoutes.HOME.toBreadcrumb(), { label: 'Autores' }];
     this._jumbotronService.inserirDados({
       titulo: 'Autores',
@@ -27,10 +30,5 @@ export class AutoresComponent implements OnDestroy {
       dataEdicao: undefined,
       tempoLeitura: undefined,
     });
-  }
-
-  ngOnDestroy(): void {
-    this._destroy$.next(true);
-    this._destroy$.unsubscribe();
   }
 }
--- projects/blog/src/app/views/blog/views/contato/contato.component.ts
+++ projects/blog/src/app/views/blog/views/contato/contato.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { JonastorresRoutes } from 'projects/blog/src/app/enuns/jonastorres-routes.enum';
+import { BlogService } from 'projects/blog/src/app/services/blog/blog.service';
 import { JumbotronService } from 'projects/blog/src/app/services/jumbotron/jumbotron.service';
+import { MasterBaseComponent } from 'projects/guide-dog/src/lib/components/master-base/master-base.component';
 import { BreadcrumbsItem } from 'projects/guide-dog/src/lib/types/breadcrumbs-item.type';
 
 @Component({
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.scss'],
 })
-export class ContatoComponent {
+export class ContatoComponent extends MasterBaseComponent {
   breadcrumbsItem: BreadcrumbsItem[];
 
-  constructor(private _jumbotronService: JumbotronService) {
+  constructor(
+    private _jumbotronService: JumbotronService,
+    private _blogService: BlogService,
+    protected override injector: Injector
+  ) {
+    super(injector);
+    this._blogService.tornarBoxPrincipalTransparente(true);
     this.breadcrumbsItem = [JonastorresRoutes.HOME.toBreadcrumb()];
     this._jumbotronService.inserirDados({
       titulo: 'Contato',
