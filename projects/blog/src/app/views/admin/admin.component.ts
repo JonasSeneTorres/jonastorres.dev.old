fc3e0f20548296a43e48b27cece9732843c0ad57
Jonas Sene Torres <jonas.sene.torres@gmail.com>
2024-06-20 11:01:03 -0300

    fix: cores de contrastes nas paginas de artigos

--- projects/blog/src/app/views/admin/admin.component.ts
+++ projects/blog/src/app/views/admin/admin.component.ts
@@ -1,10 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MasterBaseComponent } from 'projects/guide-dog/src/lib/components/master-base/master-base.component';
+import { BreadcrumbsItem } from 'projects/guide-dog/src/lib/types/breadcrumbs-item.type';
 
 @Component({
   selector: 'jt-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
-export class AdminComponent {
-  constructor() {}
+export class AdminComponent extends MasterBaseComponent implements OnInit, OnDestroy {
+  breadcrumbsItem: BreadcrumbsItem[];
+
+  constructor(
+    // private _artigosService: ArtigosService,
+    // private _categoriasService: CategoriasService,
+    // private _autoresService: AutoresService,
+    // private _blogService: BlogService,
+    // private _jumbotronService: JumbotronService,
+    // private _activatedRoute: ActivatedRoute,
+    private router: Router,
+    protected override injector: Injector
+  ) {
+    super(injector);
+    // ) {
+
+    // this._blogService.tornarBoxPrincipalTransparente(true);
+    this.breadcrumbsItem = [];
+  }
 }
